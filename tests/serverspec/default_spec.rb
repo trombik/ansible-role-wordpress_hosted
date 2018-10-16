bec68a9eb6e67d250771aa4a4a1e675599c87e69
Tomoyuki Sakurai <y@trombik.org>
2018-10-16 21:54:43 +0900

    initial import

---
+++ tests/serverspec/default_spec.rb
@@ -0,0 +1,58 @@
+require "spec_helper"
+require "serverspec"
+
+package = "wordpress_hosted"
+service = "wordpress_hosted"
+config  = "/etc/wordpress_hosted/wordpress_hosted.conf"
+user    = "wordpress_hosted"
+group   = "wordpress_hosted"
+ports   = [PORTS]
+log_dir = "/var/log/wordpress_hosted"
+db_dir  = "/var/lib/wordpress_hosted"
+
+case os[:family]
+when "freebsd"
+  config = "/usr/local/etc/wordpress_hosted.conf"
+  db_dir = "/var/db/wordpress_hosted"
+end
+
+describe package(package) do
+  it { should be_installed }
+end
+
+describe file(config) do
+  it { should be_file }
+  its(:content) { should match Regexp.escape("wordpress_hosted") }
+end
+
+describe file(log_dir) do
+  it { should exist }
+  it { should be_mode 755 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+end
+
+describe file(db_dir) do
+  it { should exist }
+  it { should be_mode 755 }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+end
+
+case os[:family]
+when "freebsd"
+  describe file("/etc/rc.conf.d/wordpress_hosted") do
+    it { should be_file }
+  end
+end
+
+describe service(service) do
+  it { should be_running }
+  it { should be_enabled }
+end
+
+ports.each do |p|
+  describe port(p) do
+    it { should be_listening }
+  end
+end
