c32dab2dcdec579f5125ee7a532e1f41801513b0
Tomoyuki Sakurai <y@trombik.org>
2018-10-17 12:02:13 +0900

    feature: generate salts from wordpress API, remove wordpress_hosted_security_keys
    also:
    
    * make wp-config.php a valid PHP file
    * add $table_prefix to wp-config.php

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -8,6 +8,8 @@ document_root = "#{home}/web"
 app_root = "#{document_root}"
 plugin_dir = "#{app_root}/wp-content/plugins"
 config  = "#{app_root}/wp-config.php"
+salts_file = "#{app_root}/wp-salts.php"
+
 plugins = [
   { name: "code-snippets" },
   { name: "dark-mode", version: "3.0.1" }
@@ -22,6 +24,17 @@ describe file(config) do
   it { should be_mode 440 }
   its(:content) { should match(/^\/\/ Managed by ansible/) }
   its(:content) { should match(/^define\(\s*'DB_PASSWORD', 'PassWord'\s*\);/) }
+  its(:content) { should match(/^\$table_prefix\s*=\s*'wp1_';$/) }
+end
+
+describe file salts_file do
+  it { should exist }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 440 }
+  %w[ AUTH_KEY SECURE_AUTH_KEY LOGGED_IN_KEY NONCE_KEY AUTH_SALT SECURE_AUTH_SALT LOGGED_IN_SALT NONCE_SALT ].each do |salt|
+    its(:content) { should match(/^define\('#{salt}',\s*'[^']+'\)/) }
+  end
 end
 
 plugins.each do |p|
